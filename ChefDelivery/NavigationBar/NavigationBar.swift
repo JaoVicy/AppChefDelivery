78d5410d0be36f931ebee285d92e901b71d8555a
JaoVicy <filipimjoa@gmail.com>
2025-07-07 16:41:11 -0300

    Create the NavigationBar UiView

---
+++ ChefDelivery/NavigationBar/NavigationBar.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationBar.swift
+//  ChefDelivery
+//
+//  Created by João Víctor Benetti Filipim on 07/07/25.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    NavigationBar()
+}
