ae7b18f25d9661b01d11482a5cd54f3d16e57d0e
JaoVicy <filipimjoa@gmail.com>
2025-07-07 15:28:23 -0300

    Initial Commit

---
+++ ChefDelivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChefDelivery
+//
+//  Created by João Víctor Benetti Filipim on 07/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ChefDeliveryTests/ChefDeliveryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryTests.swift
+//  ChefDeliveryTests
+//
+//  Created by João Víctor Benetti Filipim on 07/07/25.
+//
+
+import Testing
+@testable import ChefDelivery
+
+struct ChefDeliveryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
