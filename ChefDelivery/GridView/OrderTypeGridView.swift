119415a3167e4c3f459a49c8596b84dcd3a84f5a
JaoVicy <filipimjoa@gmail.com>
2025-07-09 18:57:12 -0300

    Created GridView folder and OrderTypeGridView

---
+++ ChefDelivery/GridView/OrderTypeGridView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderTypeGridView.swift
+//  ChefDelivery
+//
+//  Created by João Víctor Benetti Filipim on 09/07/25.
+//
+
+import SwiftUI
+
+struct OrderTypeGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderTypeGridView()
+}
