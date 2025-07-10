d5a4a9ddf9f5081f33b317762ed9834b045ef3cc
JaoVicy <filipimjoa@gmail.com>
2025-07-10 09:07:56 -0300

    Created LazyHGrid

--- ChefDelivery/GridView/OrderTypeGridView.swift
+++ ChefDelivery/GridView/OrderTypeGridView.swift
@@ -9,14 +9,11 @@ import SwiftUI
 
 struct OrderTypeGridView: View {
     var body: some View {
-        Grid {
-            GridRow {
-                Text("kjkj")
-                Text("kjkj")
-                Text("kjkj")
-            }
-            Divider()
-        }
+        LazyHGrid(rows: [
+            
+        ], content: {
+            
+        })
     }
 }
 
