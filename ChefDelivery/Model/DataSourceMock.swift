503d0eec58e137bdcaf202ad1a6228378e5a7cd5
JaoVicy <filipimjoa@gmail.com>
2025-07-10 09:40:25 -0300

    Created orderType in DataSourceMock

---
+++ ChefDelivery/Model/DataSourceMock.swift
@@ -0,0 +1,12 @@
+//
+//  DataSourceMock.swift
+//  ChefDelivery
+//
+//  Created by João Víctor Benetti Filipim on 10/07/25.
+//
+
+import Foundation
+
+let ordersMock: [OrderType] = [
+    OrderType(id: <#T##Int#>, name: "Restaurante", image:)
+]
