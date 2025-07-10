c44b46cfaf9ab1e908a6b93c55f190db608ae42a
JaoVicy <filipimjoa@gmail.com>
2025-07-10 14:06:32 -0300

    Added OrderTypes

--- ChefDelivery/Model/DataSourceMock.swift
+++ ChefDelivery/Model/DataSourceMock.swift
@@ -8,5 +8,11 @@
 import Foundation
 
 let ordersMock: [OrderType] = [
-    
+    OrderType(id: 1, name: "Restaurante", image: "hamburguer"),
+    OrderType(id: 2, name: "Mercado", image: "mercado"),
+    OrderType(id: 3, name: "Farmácia", image: "farmácia"),
+    OrderType(id: 4, name: "Pet", image: "petshop"),
+    OrderType(id: 5, name: "Descontos", image: "descontos"),
+    OrderType(id: 6, name: "Bebidas", image: "bebidas"),
+    OrderType(id: 7, name: "Gourmet", image: "gourmet")
 ]
