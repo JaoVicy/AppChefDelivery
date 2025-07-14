504550d9705f2788849d77b7f5f5b71d31626f8c
JaoVicy <filipimjoa@gmail.com>
2025-07-14 16:49:47 -0300

    Created OrderTypeView

---
+++ ChefDelivery/GridView/OrderTypeView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderTypeView.swift
+//  ChefDelivery
+//
+//  Created by João Víctor Benetti Filipim on 14/07/25.
+//
+
+import SwiftUI
+
+struct OrderTypeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderTypeView()
+}
