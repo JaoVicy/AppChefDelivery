153795bcb8ad70032054de5663c272e269740352
JaoVicy <filipimjoa@gmail.com>
2025-07-15 16:25:28 -0300

    feat: created StoreItemView

---
+++ ChefDelivery/StoresView/StoreItemView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreItemView.swift
+//  ChefDelivery
+//
+//  Created by João Víctor Benetti Filipim on 15/07/25.
+//
+
+import SwiftUI
+
+struct StoreItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreItemView()
+}
