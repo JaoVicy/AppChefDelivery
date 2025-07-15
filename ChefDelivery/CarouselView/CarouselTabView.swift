ba1c75648e779cd252ecb13da371dc05fbcd0d0d
JaoVicy <filipimjoa@gmail.com>
2025-07-15 15:59:35 -0300

    chore: created TabView

--- ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -16,7 +16,9 @@ struct CarouselTabView: View {
     ]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            
+        }
     }
 }
 
