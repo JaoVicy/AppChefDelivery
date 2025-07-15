dd71d20fb011ecd4cd6df1d812f665307b4e0932
JaoVicy <filipimjoa@gmail.com>
2025-07-15 16:20:08 -0300

    feat: added CarouselTabView()

--- ChefDelivery/App/ContentView.swift
+++ ChefDelivery/App/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack {
                 OrderTypeGridView()
+                CarouselTabView()
             }
         }
     }
