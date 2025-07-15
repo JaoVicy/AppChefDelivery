d3ea23fb7382d37b06d27a036814617081acc61e
JaoVicy <filipimjoa@gmail.com>
2025-07-15 14:45:45 -0300

    style: OrderTypeGridView()

--- ChefDelivery/App/ContentView.swift
+++ ChefDelivery/App/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
         
         ScrollView(.vertical, showsIndicators: false) {
             VStack {
-                
+                OrderTypeGridView()
             }
         }
     }
