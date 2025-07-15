b5f1d749a52a104eb251e337eb432af817019799
JaoVicy <filipimjoa@gmail.com>
2025-07-15 15:58:10 -0300

    chore: added mock

---
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -0,0 +1,25 @@
+//
+//  CarouselTabView.swift
+//  ChefDelivery
+//
+//  Created by João Víctor Benetti Filipim on 15/07/25.
+//
+
+import SwiftUI
+
+struct CarouselTabView: View {
+    
+    let orderMock: [OrderType] = [
+        OrderType(id: 1, name: "banner burguer", image: "barbecue-banner"),
+        OrderType(id: 2, name: "banner prato feito", image: "barbecue-banner"),
+        OrderType(id: 3, name: "banner poke", image: "barbecue-banner")
+    ]
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselTabView()
+}
