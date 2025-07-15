7d4d131c37ca1fd112f94a57148736646ecab784
JaoVicy <filipimjoa@gmail.com>
2025-07-15 16:59:22 -0300

    feat: added StoresContainerView() in ContentView

--- ChefDelivery/App/ContentView.swift
+++ ChefDelivery/App/ContentView.swift
@@ -19,6 +19,7 @@ struct ContentView: View {
             VStack(spacing: 20) {
                 OrderTypeGridView()
                 CarouselTabView()
+                StoresContainerView()
             }
         }
     }
